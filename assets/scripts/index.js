f17b02272f3895871c6684b57610cf7a0b3788b0
rosmith76 <robbe.smith@gmail.com>
2016-10-01 15:58:29 -0400

    HTML and CSS work. I should have committed when I finished HTML, but I forgot. I will continue CSS with a new branch.

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,7 +1,26 @@
 'use strict';
-
-// user require with a reference to bundle the file and use it in this file
-// var example = require('./example');
-
-// use require without a reference to ensure a file is bundled
+//
+// // user require with a reference to bundle the file and use it in this file
+// // var example = require('./example');
+//
+// // use require without a reference to ensure a file is bundled
 require('./example');
+
+
+
+// require('./calendar/api');
+// require('./auth/api');
+// require('./auth/app');
+// require('./auth/ui');
+// require('./calendar/logic');
+//
+// const ui = require('./calendar/ui');
+//
+// const authEvents = require('./auth/events.js');
+//
+// $(() => {
+//   $('container').show();
+//   $('form').hide();
+//   authEvents.addHandlers();
+//   ui.addHandlers();
+// });
